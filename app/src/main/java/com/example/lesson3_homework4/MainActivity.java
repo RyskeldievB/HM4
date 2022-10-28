8aac4bee17b0ac646a08e113efdb6dc9d825ba39
RyskeldievB <ryskeldievbajel014@gmail.com>
2022-10-29 04:28:02 +0600

    Initial commit

---
+++ app/src/main/java/com/example/lesson3_homework4/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.lesson3_homework4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.lesson3_homework4.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        if (savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().add(R.id.container, new MainFragment()).commit();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/lesson3_homework4/OnItemClick.java
@@ -0,0 +1,5 @@
+package com.example.lesson3_homework4;
+
+public interface OnItemClick {
+    void onItemClick(Country country);
+}
